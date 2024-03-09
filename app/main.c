2df8dad7dbcceb5103ed3b961915cb773cb2786a
Egor <evarshutin@mail.ru>
2024-03-09 22:37:12 +0300

    mainc

--- app/main.c
+++ app/main.c
@@ -8,25 +8,26 @@ int main() {
 
     // Ввод коэффициентов
     printf("Введите коэффициенты квадратного уравнения (a, b, c): ");
-    if (scanf("%f %f %f", &a, &b, &c)) {}
 
+    if (scanf("%f %f %f", &a, &b, &c) == 3) {
+    // Все три коэффициента были успешно считаны
     // Вызов функции для решения уравнения
-    num_roots = solve_quadratic(a, b, c, &x1, &x2);
-
+        num_roots = solve_quadratic(a, b, c, &x1, &x2);
     // Вывод результатов
-    if (num_roots == 2) {
-        printf("Корни уравнения: %.2f и %.2f\n", x1, x2);
-    } else if (num_roots == 1) {
-        printf("Уравнение имеет один корень: %.2f\n", x1);
-    } else if (num_roots == -1) {
-        printf("Error\n");
-    } else if (num_roots == -2) {
-        printf("Уравнение будет равно нулю\n");
-    } else if (num_roots == -3) {
-        printf("Корни уравнения: %.2f и %.2f\n", x1, x2);
+        if (num_roots == 2) {
+            printf("Корни уравнения: %.2f и %.2f\n", x1, x2);
+        } else if (num_roots == 1) {
+            printf("Уравнение имеет один корень: %.2f\n", x1);
+        } else if (num_roots == -1) {
+            printf("Error\n");
+        } else if (num_roots == -2) {
+            printf("Уравнение будет равно нулю\n");
+        } else if (num_roots == -3) {
+            printf("Корни уравнения: %.2f и %.2f\n", x1, x2);
+        } else {
+            printf("Уравнение не имеет действительных корней (Не является кв. ур.).\n");
+        }
     } else {
-        printf("Уравнение не имеет действительных корней (Не является кв. ур.).\n");
+        printf("Ошибка при вводе коэффициентов.\n");
     }
-
-    return 0;
 }
